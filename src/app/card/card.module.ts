189a3f3005f1de61b4611df3ba4dde479c808ea8
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-06-14 14:20:52 -0400

    Created CardModule, Services, Containers, Components, and Models

---
+++ src/app/card/card.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardListComponent } from './containers/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+
+@NgModule({
+  declarations: [CardListComponent, CardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CardModule { }
